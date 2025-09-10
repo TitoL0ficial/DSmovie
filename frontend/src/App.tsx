98a72f888abc02da6340c8c623483c7fa1e04a3f
Tito0ficial <titol.oficial@gmail.com>
2025-09-10 10:57:16 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 
 function App() {
   return (
-    <NavBar/>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Listing />}/>
+        <Route path="/form">
+          <Route path=":modieId" element={<Form />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
